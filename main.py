2b6b3885cdbf11151bda1b9c58f582ffc5c444a6
mrtheyann <darklodr@gmail.com>
2017-05-19 23:49:40 +0600

    upd: artists

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
+# -*- coding: utf-8 -*-
 #!/usr/local/bin/env python3
+
+#fuzzy as hell, I hope that no one ever would find this piece of code
 
 import pylast
 import sys
 
-API_KEY = "api_key"
-API_SECRET = "api_secret"
+API_KEY = "API_KEY"
+API_SECRET = "API_SECRET"
 
-username = "your_user_name"
-password_hash = pylast.md5("your_password")
-
+username = "username"
+password_hash = pylast.md5("password")
 
 network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET,
                                username=username, password_hash=password_hash)
 
+user = network.get_user(username)
 
-def get_tracks(username):
-  user = network.get_user(username)
-  return user.get_recent_tracks(limit=50, cacheable=True)
-
+#temporal unicode printer
+def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
+    enc = file.encoding
+    if enc == 'UTF-8':
+        print(*objects, sep=sep, end=end, file=file)
+    else:
+        f = lambda obj: str(obj).encode(enc, errors='backslashreplace').decode(enc)
+        print(*map(f, objects), sep=sep, end=end, file=file)
 
 def main():
-  tracks = get_tracks(sys.argv[1])
-  print(tracks)
+  library = user.get_library()
+  artists = library.get_artists(limit=50)
+  for item in artists:
+    uprint(item[0])
 
 
 if __name__ == '__main__':
